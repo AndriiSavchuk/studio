9eddbd871d2a7147b83b34bcc265a4437fc3acfa
Andrii Savchuk <andriksr@gmail.com>
2016-06-07 23:12:52 +0300

    Added fixed header during scrolling

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
+//Preloader
+
 $(window).load(function() {
 	$(".loader_inner").fadeOut();
 	$(".loader").delay(400).fadeOut("slow");
+});
+
+//Fixed header
+
+$(document).ready(function(){	
+	$("#header").removeClass("default");
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 20) {
+			$("#header").addClass("default").fadeIn('fast');
+		} else {
+			$("#header").removeClass("default").fadeIn('fast');
+		};
+	});
 });
